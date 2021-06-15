abd74193785e62124d768807d58ff66a5ae8e0e7
Jorge <jorgegvillanueva@gmail.com>
2021-06-15 11:53:53 +0200

    initial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ListaDivisas titulo="Lista divisas!!!" />
+  </div>
+</template>
+
+<script>
+import ListaDivisas from "./components/ListaDivisas";
+
+export default {
+  name: 'App',
+  components: {
+    ListaDivisas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListaDivisas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="lista-divisas">
+        <h1>{{titulo}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ListaDivisas',
+        props: {
+            titulo: String
+        }
+    }
+</script>
+
+<style>
+
+</style>
